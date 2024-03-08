886f8ba658c7c114f4f82a54d42dc037d7acecf0
Renato Soriano <renatosd_19@hotmail.com>
2024-03-08 13:30:50 -0800

    Updated README file, code and results.

---
+++ Target_Workspace/M2P_UART2_SCRATCH/Debug/Src/main.d
@@ -0,0 +1,10 @@
+Src/main.o: ../Src/main.c ../Inc/stm32f446xx.h ../Inc/core_cm4.h \
+ ../Inc/cmsis_version.h ../Inc/cmsis_compiler.h ../Inc/cmsis_gcc.h \
+ ../Inc/mpu_armv7.h ../Inc/system_stm32f4xx.h
+../Inc/stm32f446xx.h:
+../Inc/core_cm4.h:
+../Inc/cmsis_version.h:
+../Inc/cmsis_compiler.h:
+../Inc/cmsis_gcc.h:
+../Inc/mpu_armv7.h:
+../Inc/system_stm32f4xx.h:
---
+++ Target_Workspace/M2P_UART2_SCRATCH/Debug/Src/stm32f4xx_it.d
@@ -0,0 +1,10 @@
+Src/stm32f4xx_it.o: ../Src/stm32f4xx_it.c ../Inc/stm32f446xx.h \
+ ../Inc/core_cm4.h ../Inc/cmsis_version.h ../Inc/cmsis_compiler.h \
+ ../Inc/cmsis_gcc.h ../Inc/mpu_armv7.h ../Inc/system_stm32f4xx.h
+../Inc/stm32f446xx.h:
+../Inc/core_cm4.h:
+../Inc/cmsis_version.h:
+../Inc/cmsis_compiler.h:
+../Inc/cmsis_gcc.h:
+../Inc/mpu_armv7.h:
+../Inc/system_stm32f4xx.h:
